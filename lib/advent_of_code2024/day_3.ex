34c503965a751b88aeb3e675319f48766289ed8d
Paulo Henrique da Silva <paulo.silva1337@gmail.com>
2024-12-05 09:16:53 -0300

    Day 3 - Second solution.

--- lib/advent_of_code2024/day_3.ex
+++ lib/advent_of_code2024/day_3.ex
@@ -6,11 +6,38 @@ defmodule AdventOfCode2024.Day3 do
   def run(input) do
     result =
       input
+      |> reject_disabled_data()
       |> parse_input()
       |> sum_multis(0)
 
     {:ok, result}
   end
+
+  defp reject_disabled_data(input) do
+    input
+    |> String.split(~r/do\(\)|don't\(\)/, include_captures: true)
+    |> reject_disabled_data(true)
+  end
+
+  defp reject_disabled_data(captures, enabled?, acc \\ "")
+
+  defp reject_disabled_data(["don't()" | rest], true, acc) do
+    reject_disabled_data(rest, false, acc)
+  end
+
+  defp reject_disabled_data(["do()" | rest], false, acc) do
+    reject_disabled_data(rest, true, acc)
+  end
+
+  defp reject_disabled_data([enabled_data | rest], true, acc) do
+    reject_disabled_data(rest, true, acc <> enabled_data)
+  end
+
+  defp reject_disabled_data([_ignore | rest], false, acc) do
+    reject_disabled_data(rest, false, acc)
+  end
+
+  defp reject_disabled_data([], _, acc), do: acc
 
   @mul_regex ~r/mul\((\d+),(\d+)\)/
   defp parse_input(input) do
--- test/advent_of_code2024/day_3_test.exs
+++ test/advent_of_code2024/day_3_test.exs
@@ -6,8 +6,8 @@ defmodule AdventOfCode2024.Day3Test do
 
   describe "run/1" do
     test "counts how many reports are safe" do
-      input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-      assert Day3.run(input) == {:ok, 161}
+      input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+      assert Day3.run(input) == {:ok, 48}
     end
   end
 end
