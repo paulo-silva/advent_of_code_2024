53ef6e73aa72257d489c967d232cb15ef40e7edd
Paulo Henrique da Silva <paulo.silva1337@gmail.com>
2024-12-10 01:57:07 -0300

    [WIP] Day 9 - Second solution.

--- test/advent_of_code2024/day_9_test.exs
+++ test/advent_of_code2024/day_9_test.exs
@@ -11,9 +11,9 @@ defmodule AdventOfCode2024.Day9Test do
       assert Day9.run(input) == {:ok, 1928}
     end
 
-    test "part 1 result" do
+    test "part 2 result" do
       input = 9 |> AdventOfCode2024.input_file() |> File.read!()
-      assert Day9.run(input) == {:ok, 6_398_608_069_280}
+      assert Day9.run(input) == {:ok, 6_427_437_134_372}
     end
   end
 end
